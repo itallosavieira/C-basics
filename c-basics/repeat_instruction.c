12bd69f42632ba18eeede772b792e89f33abcb85
itallosavieira <itallo.sa.vieira@gmail.com>
2021-03-28 12:01:11 -0300

    add repeat_instruction

--- c-basics/repeat_instruction.c
+++ c-basics/repeat_instruction.c
@@ -3,5 +3,17 @@
 
 int main()
 {
-  
+  int x = 0;
+  int val_typed = 0;
+
+  printf("Type the times the instruction will be repeated: ");
+  scanf("%i", &val_typed);
+
+  while(x <= val_typed)
+  {
+    printf("%i\n", x * 10);
+    x++;
+  }
+
+return 0;
 }
